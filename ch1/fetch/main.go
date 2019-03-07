7cb6aa3423cc4989abf820bcd42881c0c14ba3e5
Stephen French <stephenfrench9@gmail.com>
2019-03-07 09:16:00 -0800

    play around with the 'Response' type in the http package

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -12,22 +12,32 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"reflect"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
 		resp, err := http.Get(url)
+
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			_, _ = fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
+		fmt.Print("Resp Type: ")
+		fmt.Println(reflect.TypeOf(resp.Header))
+		fmt.Print("Status: ")
+		fmt.Println(resp.Status)
+		fmt.Println("Proto: ")
+		fmt.Println(resp.Proto)
 		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
+		_ = resp.Body.Close()
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			_, _ = fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+
+		fmt.Printf("%s", b[:50])
+		fmt.Println(reflect.TypeOf(b))
 	}
 }
 
