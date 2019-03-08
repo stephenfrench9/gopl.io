4dc257f8cc1b5fabbd4b747706c01ae171ef6c29
Stephen French <stephenfrench9@gmail.com>
2019-03-07 23:47:01 -0800

    exercises 1.8 and 1.9

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -13,10 +13,18 @@ import (
 	"net/http"
 	"os"
 	"reflect"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if strings.HasPrefix(url,"http://") {
+			fmt.Println("it don't have the prefix!!, lets fix")
+			url = "http://" + url
+		} else {
+			fmt.Println("it do got the prefix")
+		}
+
 		resp, err := http.Get(url)
 
 		if err != nil {
@@ -24,19 +32,27 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Print("Resp Type: ")
+		fmt.Println(reflect.TypeOf(resp))
+		fmt.Print("resp.Header Type: ")
 		fmt.Println(reflect.TypeOf(resp.Header))
 		fmt.Print("Status: ")
 		fmt.Println(resp.Status)
 		fmt.Println("Proto: ")
 		fmt.Println(resp.Proto)
+		fmt.Println("resp.Body type")
+		fmt.Println(reflect.TypeOf(resp.Body))
+
 		b, err := ioutil.ReadAll(resp.Body)
+		//length, err := io.Copy(os.Stdout, resp.Body)
 		_ = resp.Body.Close()
 		if err != nil {
 			_, _ = fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
+		//fmt.Printf("type: %v and value: %s",reflect.TypeOf(length), length)
+		//fmt.Printf("type: %v and value: %v",reflect.TypeOf(err), err)
 
-		fmt.Printf("%s", b[:50])
+		fmt.Printf("%s\n", b[:50])
 		fmt.Println(reflect.TypeOf(b))
 	}
 }
