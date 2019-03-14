a807627b43f5c6042690db3ddc0c423b18659d3e
Stephen French <stephenfrench9@gmail.com>
2019-03-14 14:54:31 -0700

    ch4/sha256

--- ch4/sha256/main.go
+++ ch4/sha256/main.go
@@ -6,15 +6,29 @@
 // The sha256 command computes the SHA256 hash (an array) of a string.
 package main
 
-import "fmt"
+//Stephen: You can format outputs by using fmt.Printf, and then you put a number after the percent sign.
+//Stephen: A uint8 can be represented as a hexint, or as a string.
+//Stephen: you can have an array of unint8s.
+//Stephen: is == superficial? It seems like yes it is, it checks values, not the underlying object.
 
-//!+
-import "crypto/sha256"
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
 
 func main() {
+	//c1 := "x"
+	//c2 := "X"
+	//c1 := []byte("x")
+	//c2 := []byte("X")
+	//c1 := []byte("x")[1]
+	//c2 := []byte("X")[1]
 	c1 := sha256.Sum256([]byte("x"))
-	c2 := sha256.Sum256([]byte("X"))
-	fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
+	c2 := sha256.Sum256([]byte("x"))
+	fmt.Printf("string: %4s\nstring: %4s\nhex int: %4x\nhex int: %4x\nSame: %7t\ntype: %7T\n", c1, c2, c1, c2, c1 == c2, c1)
+
+
 	// Output:
 	// 2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881
 	// 4b68ab3847feda7d6c62c1fbcbeebfa35eab7351ed5e78f4ddadea5df64b8015
@@ -23,3 +37,4 @@ func main() {
 }
 
 //!-
+
