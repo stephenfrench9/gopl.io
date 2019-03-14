fa09575f22cbec3b20977a1da356e4ed835a5ced
Stephen French <stephenfrench9@gmail.com>
2019-03-14 13:59:02 -0700

    play around with ch1/server3

--- ch1/server3/main.go
+++ ch1/server3/main.go
@@ -20,18 +20,23 @@ func main() {
 //!+handler
 // handler echoes the HTTP request.
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+
+	_, _ = fmt.Fprintf(w, "ttt%s %s %s\n", r.Method, r.URL, r.Proto)
+
+
 	for k, v := range r.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		_, _ = fmt.Fprintf(w, "AHeader[%q] = %q\n", k, v)
 	}
-	fmt.Fprintf(w, "Host = %q\n", r.Host)
-	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	_, _ = fmt.Fprintf(w, "Host = %q\n", r.Host)
+	_, _ = fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
 	if err := r.ParseForm(); err != nil {
 		log.Print(err)
 	}
 	for k, v := range r.Form {
-		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+		_, _ = fmt.Fprintf(w, "EVER? Form[%q] = %q\n", k, v)
 	}
 }
 
 //!-handler
+
+//Examine all the headers in the request, also the browser is telling the server what address the browser is at.
